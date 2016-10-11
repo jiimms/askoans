e345e5a66978012bde0f660e5039b3a51345c787
jiimms <jiimms@yahoo.com>
2016-10-11 23:46:00 +0530

    carrierwave done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	mount_uploader :image, ImageUploader
 	has_secure_password
 	validates :name, presence: true
 	validates :email, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005140307) do
+ActiveRecord::Schema.define(version: 20161010165905) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "response"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161005140307) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
