463874fd958802c24a07fa589394f1cf5f48e3e0
jiimms <jiimms@yahoo.com>
2016-10-18 01:48:10 +0530

    fog done partly, resizing done

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'carrierwave', '~> 0.10.0'
 gem 'mini_magick', '~> 4.3'
+gem 'fog'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,1 +1,12 @@
 require 'carrierwave/orm/activerecord'
+
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => 'AKIAIKXAZWCFKZRCMIYQ',
+      :aws_secret_access_key => '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',
+      :region => 'us-west-2'
+    }
+    config.fog_directory     =  'askons-assets'
+  end
